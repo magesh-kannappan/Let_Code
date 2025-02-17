e8842c1dd9aded67329777804e7eee13fba1c85b
Magesh <mageshtesting2022@gmail.com>
2025-02-17 18:40:08 +0530

    Practice_17_02_25

--- src/main/java/a2_Pages/Input_Page.java
+++ src/main/java/a2_Pages/Input_Page.java
@@ -1,13 +1,22 @@
 package a2_Pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Input_Page {
 
 	public WebDriver driver;
 
-	Input_Page(WebDriver driver) {
-
+	Input_Page(WebDriver driver2) {
+		this.driver = driver2;
+		PageFactory.initElements(driver, this);
 	}
 
+	private void asd() {
+		// TODO Auto-generated method stub
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+	}
 }
