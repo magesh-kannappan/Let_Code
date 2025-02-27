289c39e0ec80e76c5d87ff1cb9da2daedda2ddaf
Magesh <mageshtesting2022@gmail.com>
2025-02-27 22:24:40 +0530

    Practice_27_02_2025

--- src/test/java/trial_Run/WindowHandling.java
+++ src/test/java/trial_Run/WindowHandling.java
@@ -1,11 +1,35 @@
 package trial_Run;
 
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class WindowHandling {
+
+	private void asd() {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.netmeds.com");
+
+		// JavascriptExecutor js = (JavascriptExecutor) driver;
+		// js.executeScript("arguments[0].click();", "element");
+		// js.executeScript("arguments[0].scrollIntoView(true);", "element");
+
+		float percent = 123.21f;
+		int mrp = (int) percent;
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.switchTo().parentFrame();
+		driver.switchTo().defaultContent();
+
+
+	}
 
 	@Test
 	void handle() {
@@ -13,7 +37,11 @@ public class WindowHandling {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.netmeds.com");
 		driver.switchTo().newWindow(WindowType.TAB);
-		driver.get("https://www.amazon.com");
+		driver.switchTo().newWindow(WindowType.TAB);
+		driver.switchTo().window(null);
+
+		// JavascriptExecutor js = (JavascriptExecutor) driver;
+		// js.executeScript("arguments[0].click();", "element");
 
 	}
 
